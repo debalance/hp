747c8c939f10076e9a4991d3f3543c529add5270
Philipp Huebner <debalance@debian.org>
2017-01-17 14:53:22 +0100

    check auth, implement leave group + delete group

--- hp/groups/views.py
+++ hp/groups/views.py
@@ -1,5 +1,7 @@
 import logging
+log = logging.getLogger(__name__)
 
+from django import forms
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import get_user_model
@@ -11,6 +13,7 @@ from django.core.urlresolvers import reverse
 from django.core.urlresolvers import reverse_lazy
 from django.db import transaction
 from django.http import HttpResponse
+from django.http import HttpResponseForbidden
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.shortcuts import resolve_url
@@ -19,13 +22,15 @@ from django.utils import timezone
 from django.utils.http import is_safe_url
 from django.utils.translation import ugettext_lazy as _
 from django.utils.translation import ugettext_noop
-from django.views.generic import View
 from django.views.generic import DetailView
+from django.views.generic import FormView
 from django.views.generic.base import RedirectView
 from django.views.generic.base import TemplateView
 from django.views.generic.detail import SingleObjectMixin
 
 from .models import Group
+from .models import membership
+from .models import ownership
 from account.models import User
 from account.models import Confirmation
 from account.views import UserObjectMixin
@@ -33,11 +38,9 @@ from core.views import StaticContextMixin
 from xmpp_backends.django import xmpp_backend
 
 
-User = get_user_model()
-log = logging.getLogger(__name__)
-_confirmation_qs = Confirmation.objects.valid().select_related('user')
-
-
+#
+# context menu
+#
 class GroupPageMixin(StaticContextMixin):
     """Mixin that adds the groupmenu on the left to views where the user is logged in."""
 
@@ -91,7 +94,43 @@ class GroupPageMixin(StaticContextMixin):
         return context
 
 
-class OverView(LoginRequiredMixin, GroupPageMixin, UserObjectMixin, TemplateView):
+#
+# helper mixins
+#
+class GroupAuthMixin():
+    """Mixin that adds authorization information for Group"""
+
+    def authorized_to_edit(self):
+        user = self.request.user
+        group = get_object_or_404(Group, pk=self.kwargs['pk'])
+        if group in user.owner.all():
+            return True
+        else:
+            return False
+    
+    def authorized_to_leave(self):
+        user = self.request.user
+        group = get_object_or_404(Group, pk=self.kwargs['pk'])
+        if group in user.member.all():
+            return True
+        else:
+            return False
+    
+    def authorized(self):
+        user = self.request.user
+        group = get_object_or_404(Group, pk=self.kwargs['pk'])
+        if group in user.owner.all():
+            return True
+        elif group in user.member.all():
+            return True
+        else:
+            return False
+
+
+#
+# menu items
+#
+class OverView(LoginRequiredMixin, GroupPageMixin, TemplateView):
     """Main group settings view (/groups)."""
     template_name = 'groups/overview.html'
     groupmenu_item = 'groups:overview'
@@ -114,27 +153,54 @@ class CreateView(LoginRequiredMixin, GroupPageMixin, TemplateView):
     #TODO
 
 
-class GroupView(LoginRequiredMixin, GroupPageMixin, DetailView):
-    #TODO: only allow members+owners to see group information, show edit button only to owners
+#
+# additional views
+#
+class GroupView(LoginRequiredMixin, GroupPageMixin, GroupAuthMixin, DetailView):
     model = Group
     template_name = 'groups/group.html'
-    #TODO
 
 
-class EditView(LoginRequiredMixin, GroupPageMixin, DetailView):
-    #TODO: only allow members+owners to see and edit group information
-    model = Group
-    template_name = 'groups/edit.html'
-    #TODO
-
-class LeaveView(LoginRequiredMixin, GroupPageMixin, DetailView):
-    #TODO: only allow members+owners to see and use this
+class LeaveView(LoginRequiredMixin, GroupPageMixin, GroupAuthMixin, DetailView):
     model = Group
     template_name = 'groups/leave.html'
+    
+    def post(self, request, *args, **kwargs):
+        user = self.request.user
+        group = get_object_or_404(Group, pk=self.kwargs['pk'])
+        if group in user.member.all():
+            membership.objects.filter(user=user.id,group=group.id).delete()
+            messages.success(self.request, _("You have left the group '%(group)s'.") % { 'group': group.name })
+        else:
+            messages.error(self.request, _("You are not a member of this group so you cannot leave it!"))
+        return HttpResponseRedirect(reverse('groups:membership'))
+
+
+class EditView(LoginRequiredMixin, GroupPageMixin, GroupAuthMixin, DetailView):
+    model = Group
+    template_name = 'groups/edit.html'
+
+    def authorized(self):
+        user = self.request.user
+        group = get_object_or_404(Group, pk=self.kwargs['pk'])
+        if group in user.owner.all():
+            return True
+        else:
+            return False
     #TODO
 
-class DeleteView(LoginRequiredMixin, GroupPageMixin, DetailView):
-    #TODO: only allow owners to see and use this
-    model = Group
+
+class DeleteView(LoginRequiredMixin, GroupPageMixin, GroupAuthMixin, DetailView):
     template_name = 'groups/delete.html'
-    #TODO
+    model = Group
+
+    def post(self, request, *args, **kwargs):
+        user = self.request.user
+        group = get_object_or_404(Group, pk=self.kwargs['pk'])
+        if group in user.owner.all():
+            groupname = group.name
+            group.delete()
+            messages.success(self.request, _("The group '%(group)s' has been deleted.") % { 'group': groupname })
+        else:
+            messages.error(self.request, _("You are not an owner of this group so you cannot delete it!"))
+        return HttpResponseRedirect(reverse('groups:ownership'))
